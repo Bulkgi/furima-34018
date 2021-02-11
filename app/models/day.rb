40d46cafbe8d1f3917eeaf590f21653569f2b6c4
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-02-11 18:06:41 +0900

    プルダウンメニューをActive hachを用いて作成

---
+++ app/models/day.rb
@@ -0,0 +1,8 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '１〜２日で発送' },
+    { id: 3, name: '２〜３で発送' },
+    { id: 4, name: '４〜７日で発送' }
+  ]
+  end
