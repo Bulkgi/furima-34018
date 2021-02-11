190d7b2f19c45ebce1edf3c481b0c2dae30f5d0c
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-02-11 19:10:04 +0900

    Active stratgeを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,21 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee_burden
   belongs_to :area
   belongs_to :day
+  has_one_attached :image
+
+
+  with_options presence: true do
+    validates :product
+    validates :description
+    validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id 
+    validates :state_id
+    validates :delivery_fee_burden_id 
+    validates :area_id 
+    validates :day_id
+  end
+
 end
