58bd30aaf4542cd3e42080f1ceacc1ade51ebe60
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-02-11 18:15:22 +0900

    プルダウンメニューモデルのアソシエーションを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee_burden
+  belongs_to :area
+  belongs_to :day
 end
