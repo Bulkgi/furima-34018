821971019f62a803e4fe32dfd5d1cae4d5c144d4
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-02-18 15:44:39 +0900

    購入機能のページを表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,16 @@
 class OrdersController < ApplicationController
 
   def index
-    @order_address = OrderAddress.new(order_params)
+    @order_address = OrderAddress.new
+    
+  end
+
+  def create
+    binding.pry
   end
 
   private
   def order_params
-    params.require(:postal_code, :area_id, :municipality, :address , :building , :phone).merge(user_id: current_user.id, item_id: item.id)
+    params.require(:postal_code, :area_id, :municipality, :address , :building , :phone, :order_id).merge( item_id: item.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :index, :show]
-  resources :order, only: [:index] do
-    resources :address, only: [:create]
+  resources :items, only: [:new, :create, :index, :show] do
+    resources :orders, only:[:index, :create]
   end
+  
 end
