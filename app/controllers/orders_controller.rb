4304416370d00caccb7e3281f03e787ac83a4115
Bulkgi <73314476+Bulkgi@users.noreply.github.com>
2021-02-18 18:25:19 +0900

    orderコントローラーの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,25 @@ class OrdersController < ApplicationController
 
   def index
     @order_address = OrderAddress.new
-    
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    binding.pry
+    @order_address = OrderAddress.new(address_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      
+      render :index
+    end
   end
 
   private
-  def order_params
-    params.require(:postal_code, :area_id, :municipality, :address , :building , :phone, :order_id).merge( item_id: item.id)
+  def address_params
+    params.permit(:postal_code, :area_id, :municipality, :address , :building , :phone, :order_id).merge(item_id: params[:item_id], user_id: current_user.id)
   end
+
+  
 end
